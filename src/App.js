2a6c14684331c27f384a410c3ecae9cbd852acba
DenisMiszeniow <duminocorp@gmail.com>
2022-09-06 21:04:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [state, setState] = useState (
+    {
+      myLocation: '',
+      myIp: '', 
+      myCity: '',
+      myCountry:'',
+      myCountryCode: '',
+    }
+  )
+  
+  useEffect(
+    () => {
+      axios
+      .get('http://api.ipstack.com/check?access_key=107bb5ac8b7d6154107cd79f4be2903d')
+      .then(response => {
+        const myNewData = response.data 
+        setState({...state, myLocation: `${myNewData.latitude} ${myNewData.longitude}`,
+          myIp: myNewData.ip, 
+          myCity: myNewData.city, 
+          myCountry: myNewData.country_name, 
+          myCountryCode: myNewData.country_code})
+        console.log(response.data)
+        console.log(state)
+
+      })
+      .catch(error => {console.error(error)})
+    }, [state.myLocation]
+  )
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App__LeftBar'>
+          <p>List of all searches</p>
+      </div>
+      <div className='App__Container'>
+        <div className='App__Container__Content'>
+          <div>
+              <p>Map with user location</p>
+          </div>
+          <div>
+            <div>
+            <p>Information about user location</p>
+            <span>Your Country: {state.myCountry}</span>
+            <span>Your City: {state.myCity}</span>
+            <span>Your IP: {state.myIp}</span>
+            
+            </div>
+          </div>
+        </div>
+        <div className='App__Container__Form'>
+          <input type='text' placeholder=' IP address or URL'/>
+          <input type='submit' value='SEARCH'/>
+        </div>
+        <div className='App__Container__Content'>
+          <div>
+          <p>Map with last search location</p>
+          </div>
+          <div>
+          <p>Information about last search</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
